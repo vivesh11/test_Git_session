3598fdd8bbc54b617576622c30896f0ed257943a
Ankit Kumar <aktetri@gmail.com>
2024-12-28 21:24:01 +0530

    issue generator

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ print("Vivesh Devops Engineer")
 print("Vivesh Devops Engineer in Bangalore")
 print("Vivesh Devops Engineer in Bangalore, India")
 print("Vivesh Devops Engineer in Bangalore, India, Asia")
+
 print("Ankit Kumar")
 print("Ankit Kumar, Software Engineer")
+
+
+def add(a,b):
+    return a+b
